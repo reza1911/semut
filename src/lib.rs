15fef80bb514d8ee64ca6b8cdcd79fbd235b7c69
invesibletint <166238198+invesibletint@users.noreply.github.com>
2025-07-27 00:58:10 +0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -18,13 +18,8 @@ static PROXYKV_PATTERN: Lazy<Regex> = Lazy::new(|| Regex::new(r"^([A-Z]{2})").un
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, _: Context) -> Result<Response> {
-    let uuid = env
-        .var("UUID")
-        .map(|x| Uuid::parse_str(&x.to_string()).unwrap_or_default())?;
-    let host = req.url()?.host().map(|x| x.to_string()).unwrap_or_default();
-    let main_page_url = env.var("MAIN_PAGE_URL").map(|x|x.to_string()).unwrap();
-    let sub_page_url = env.var("SUB_PAGE_URL").map(|x|x.to_string()).unwrap();
-    let config = Config { uuid, host: host.clone(), proxy_addr: host, proxy_port: 443, main_page_url, sub_page_url };
+    // Konfigurasi manual agar IP & Port tidak diekspose ke user
+    let config = Config::manual();
 
     Router::with_data(config)
         .on_async("/", fe)
@@ -36,77 +31,71 @@ async fn main(req: Request, env: Env, _: Context) -> Result<Response> {
 }
 
 async fn get_response_from_url(url: String) -> Result<Response> {
-    let req = Fetch::Url(Url::parse(url.as_str())?);
+    let req = Fetch::Url(Url::parse(&url)?);
     let mut res = req.send().await?;
     Response::from_html(res.text().await?)
 }
 
 async fn fe(_: Request, cx: RouteContext<Config>) -> Result<Response> {
-    get_response_from_url(cx.data.main_page_url).await
+    get_response_from_url(cx.data.main_page_url.clone()).await
 }
 
 async fn sub(_: Request, cx: RouteContext<Config>) -> Result<Response> {
-    get_response_from_url(cx.data.sub_page_url).await
+    get_response_from_url(cx.data.sub_page_url.clone()).await
 }
-
 
 async fn tunnel(req: Request, mut cx: RouteContext<Config>) -> Result<Response> {
     let mut proxyip = cx.param("proxyip").unwrap().to_string();
+
     if PROXYKV_PATTERN.is_match(&proxyip)  {
         let kvid_list: Vec<String> = proxyip.split(",").map(|s|s.to_string()).collect();
         let kv = cx.kv("AIO")?;
-        let mut proxy_kv_str = kv.get("proxy_kv").text().await?.unwrap_or("".to_string());
+        let mut proxy_kv_str = kv.get("proxy_kv").text().await?.unwrap_or_default();
         let mut rand_buf = [0u8, 1];
         getrandom::getrandom(&mut rand_buf).expect("failed generating random number");
-        
-        if proxy_kv_str.len() == 0 {
+
+        if proxy_kv_str.is_empty() {
             console_log!("getting proxy kv from github...");
             let req = Fetch::Url(Url::parse("https://raw.githubusercontent.com/datayumiwandi/shiroko/refs/heads/main/Data/Alive.json")?);
             let mut res = req.send().await?;
             if res.status_code() == 200 {
                 proxy_kv_str = res.text().await?.to_string();
-                kv.put("proxy_kv", &proxy_kv_str)?.expiration_ttl(60 * 60 * 12).execute().await?; // 12 hours
+                kv.put("proxy_kv", &proxy_kv_str)?.expiration_ttl(60 * 60 * 12).execute().await?;
             } else {
                 return Err(Error::from(format!("error getting proxy kv: {}", res.status_code())));
             }
         }
-        
+
         let proxy_kv: HashMap<String, Vec<String>> = serde_json::from_str(&proxy_kv_str)?;
-        
-        // select random KV ID
         let kv_index = (rand_buf[0] as usize) % kvid_list.len();
         proxyip = kvid_list[kv_index].clone();
-        
-        // select random proxy ip
         let proxyip_index = (rand_buf[0] as usize) % proxy_kv[&proxyip].len();
         proxyip = proxy_kv[&proxyip][proxyip_index].clone().replace(":", "-");
     }
 
     let upgrade = req.headers().get("Upgrade")?.unwrap_or_default();
-    if upgrade == "websocket".to_string() && PROXYIP_PATTERN.is_match(&proxyip) {
+    if upgrade == "websocket" && PROXYIP_PATTERN.is_match(&proxyip) {
         if let Some((addr, port_str)) = proxyip.split_once('-') {
             if let Ok(port) = port_str.parse() {
                 cx.data.proxy_addr = addr.to_string();
                 cx.data.proxy_port = port;
             }
         }
-        
+
         let WebSocketPair { server, client } = WebSocketPair::new()?;
         server.accept()?;
-    
+
         wasm_bindgen_futures::spawn_local(async move {
             let events = server.events().unwrap();
             if let Err(e) = ProxyStream::new(cx.data, &server, events).process().await {
                 console_error!("[tunnel]: {}", e);
             }
         });
-    
+
         Response::from_websocket(client)
     } else {
-        // Response::from_html("hi from wasm!")
-        get_response_from_url(cx.data.main_page_url).await
+        get_response_from_url(cx.data.main_page_url.clone()).await
     }
-
 }
 
 fn link(_: Request, cx: RouteContext<Config>) -> Result<Response> {
@@ -128,13 +117,17 @@ fn link(_: Request, cx: RouteContext<Config>) -> Result<Response> {
             "path": "/ID",
             "tls": "tls",
             "sni": host,
-            "alpn": ""}
-        );
+            "alpn": ""
+        });
         format!("vmess://{}", URL_SAFE.encode(config.to_string()))
     };
+
     let vless_link = format!("vless://{uuid}@{host}:443?encryption=none&type=ws&host={host}&path=%2FID&security=tls&sni={host}#Jatim vless");
     let trojan_link = format!("trojan://{uuid}@{host}:443?encryption=none&type=ws&host={host}&path=%2FID&security=tls&sni={host}#Jatim trojan");
-    let ss_link = format!("ss://{}@{host}:443?plugin=v2ray-plugin%3Btls%3Bmux%3D0%3Bmode%3Dwebsocket%3Bpath%3D%2FID%3Bhost%3D{host}#Jatim ss", URL_SAFE.encode(format!("none:{uuid}")));
-    
+    let ss_link = format!(
+        "ss://{}@{host}:443?plugin=v2ray-plugin%3Btls%3Bmux%3D0%3Bmode%3Dwebsocket%3Bpath%3D%2FID%3Bhost%3D{host}#Jatim ss",
+        URL_SAFE.encode(format!("none:{uuid}"))
+    );
+
     Response::from_body(ResponseBody::Body(format!("{vmess_link}\n\n{vless_link}\n\n{trojan_link}\n\n{ss_link}").into()))
 }
